8e2ec1eadbba9914b07e7df2f55fbc38bc732680
chiauk <mo850131@gmail.com>
2016-03-17 14:16:36 +0800

    exam.py

---
+++ exam.py
@@ -0,0 +1,30 @@
+#!/usr/bin/eve python
+#encoding: utf-8
+
+def isPrime(N):
+	x=1
+	sum=0
+
+	while x<=N:
+		if N%x==0:
+			sum=sum+1
+		x=x+1
+
+	global a
+	a=1
+
+	if sum==2:
+		return a
+	else:
+		a=0
+		return a
+
+print isPrime(7)
+
+total=0
+
+for x in range(2,1000):	
+	if isPrime(x)==1:
+		total=total+x
+
+print total
