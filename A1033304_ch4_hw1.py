b086432adb5fca98b838369a5f34feeff9985aaa
chiauk <mo850131@gmail.com>
2016-03-31 00:10:09 +0800

    ch4

---
+++ A1033304_ch4_hw1.py
@@ -0,0 +1,6 @@
+str="Honey, you are always in my mind.\nYour love make me blind.\nWhen I first met you I knew it was a sign"
+
+name={'Mary':'Honey','Lynn':'Baby','Jannie':'Little sweetie'}
+
+print name['Lynn'],str[5:],'\n'
+print name['Jannie'],str[5:]
